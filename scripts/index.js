24255d213cfb22078d0eb133fac10659d3c9eb8d
Haden-Brock <stix03@gmail.com>
2022-07-05 11:01:00 -0500

    make all exit buttons have common class. Make new cards prepend instead of append. Clear old inputs on add card window after card is created. change variables to const. Make functions perform single tasks. get rid of handleModalToggle function for universal functions handleModalOpen and handleModalClose. Get rid of parentElement and use .closest() for functions. create separate function for createCard and prependCard.

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,20 @@ const editButton = document.querySelector(".profile__edit");
 const addButton = document.querySelector(".profile__add");
 const editWindow = document.querySelector("#edit");
 const addCardWindow = document.querySelector("#addCard");
-const editExitButton = editWindow.querySelector(".modal__container-exit");
-const addCardExitButton = addCardWindow.querySelector(".modal__container-exit");
 const editFormElement = editWindow.querySelector(".form");
 const addFormElement = addCardWindow.querySelector(".form");
 const pictureWindow = document.querySelector("#pictureModal");
 const pictureWindowImage = pictureWindow.querySelector(".modal__picture-image");
 const pictureWindowText = pictureWindow.querySelector(".modal__picture-text");
-const pictureWindowExit = pictureWindow.querySelector(".modal__picture-exit");
-let profileName = document.querySelector(".profile__info-name");
-let profileDescription = document.querySelector(".profile__info-description");
-let nameInput = document.querySelector("#name");
-let descriptionInput = document.querySelector("#description");
-let cardTemplate = document.querySelector("#card").content;
-let elementsGrid = document.querySelector(".elements__grid");
-let titleInput = document.querySelector("#title");
-let linkInput = document.querySelector("#link");
+const profileName = document.querySelector(".profile__info-name");
+const profileDescription = document.querySelector(".profile__info-description");
+const nameInput = document.querySelector("#name");
+const descriptionInput = document.querySelector("#description");
+const cardTemplate = document.querySelector("#card").content;
+const elementsGrid = document.querySelector(".elements__grid");
+const titleInput = document.querySelector("#title");
+const linkInput = document.querySelector("#link");
+const exitButtons = document.querySelectorAll(".modal__container-exit");
 
 let cardsList = [
     {
@@ -46,31 +44,34 @@ let cardsList = [
     }
 ];
 
-function handleModalToggle (evt) {
-    let window;
-    let buttonClass = evt.target.className;   
-    if (buttonClass.includes("exit")) {
-        window = evt.target.parentElement.parentElement;
-    } else if (evt.target.className === "card__image"){
-        window = pictureWindow;
-    } else {
-        const id = evt.target.id;
-        switch (id) {
-            case "editOpen":
-            case "editSubmit":
-                window = editWindow;
-                break;
-            case "addOpen":
-            case "addSubmit":
-                window = addCardWindow;
-                break;
-        }
-    }
-    window.classList.toggle("modal_opened");
-    if (editWindow.classList.contains("modal_opened")) {
-        nameInput.value = profileName.textContent;
-        descriptionInput.value = profileDescription.textContent;
-    }
+function handleModalClose (window) {
+    window.classList.remove("modal_opened");
+}
+
+function handleModalOpen (window) {
+    window.classList.add("modal_opened");
+}
+
+function handleModalEdit () {
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
+    handleModalOpen(editWindow);
+}
+
+function handleModalAdd () {
+    titleInput.value = "";
+    linkInput.value = "";
+    handleModalOpen(addCardWindow);
+}
+
+function handleModalPicture (evt) {
+    const picture = evt.target;
+    const pictureSource = evt.target.src;
+    const pictureText = evt.target.alt;
+    pictureWindowImage.src = pictureSource;
+    pictureWindowImage.alt = pictureText;
+    pictureWindowText.textContent = pictureText;
+    handleModalOpen(pictureWindow);
 }
 
 function handleEditFormSubmit (evt) {
@@ -79,7 +80,7 @@ function handleEditFormSubmit (evt) {
     userDescription = descriptionInput.value;
     profileName.textContent = userName;
     profileDescription.textContent = userDescription;
-    handleModalToggle(evt);
+    handleModalClose(editWindow);
 }
 
 function handleAddFormSubmit (evt) {
@@ -88,9 +89,8 @@ function handleAddFormSubmit (evt) {
         name: titleInput.value,
         link: linkInput.value
     };
-    cardsList.push(card);
-    getCardElement(card);
-    handleModalToggle(evt);
+    prependCard(card);
+    handleModalClose(addCardWindow);
 }
 
 function handleLikeButton (evt) {
@@ -99,41 +99,36 @@ function handleLikeButton (evt) {
 }
 
 function handleDeleteButton (evt) {
-    evt.target.parentElement.remove();
+    evt.target.closest(".card").remove();
 }
 
-function handlePictureModal (evt) {
-    const picture = evt.target;
-    const pictureSource = evt.target.src;
-    const pictureText = evt.target.alt;
-    pictureWindowImage.src = pictureSource;
-    pictureWindowImage.alt = pictureText;
-    pictureWindowText.textContent = pictureText;
-    handleModalToggle(evt);
-}
-
-function getCardElement(data) {
+function createCard(data) {
     let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
     let cardImage = cardElement.querySelector(".card__image");
     cardImage.src = data.link;
     cardImage.alt = data.name;
     cardElement.querySelector(".card__bottom-text").textContent = data.name;
-    elementsGrid.append(cardElement);
     const likeButton = cardElement.querySelector(".card__bottom-button");
     likeButton.addEventListener("click", handleLikeButton);
     const deleteButton = cardElement.querySelector(".card__delete");
     deleteButton.addEventListener("click", handleDeleteButton);
-    cardImage.addEventListener("click", handlePictureModal);
+    cardImage.addEventListener("click", handleModalPicture);
+    return cardElement;
 }
 
-cardsList.forEach(card => {
-    getCardElement(card);
+function prependCard(data) {
+    let cardElement = createCard(data);
+    elementsGrid.prepend(cardElement);
+}
+
+cardsList.forEach(prependCard);
+
+exitButtons.forEach((button) => {
+    const window = button.closest(".modal");
+    button.addEventListener("click", () => handleModalClose(window));
 });
 
-editButton.addEventListener("click", handleModalToggle);
-addButton.addEventListener("click", handleModalToggle);
-editExitButton.addEventListener("click", handleModalToggle);
-addCardExitButton.addEventListener("click", handleModalToggle);
-pictureWindowExit.addEventListener("click", handleModalToggle);
+editButton.addEventListener("click", handleModalEdit);
+addButton.addEventListener("click", handleModalAdd);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 addFormElement.addEventListener("submit", handleAddFormSubmit);
